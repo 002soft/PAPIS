d5c29510c46c1c89d68f28cc518ef707dbf19afd
002soft <156668051+002soft@users.noreply.github.com>
2024-01-17 10:03:55 +0530

    install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+
+# install mpg321
+sudo apt-get install mpg321
+
+# install jansson library 
+sudo apt-get install libjansson-dev
+
+# install json-c/json.h library 
 sudo apt install libjson-c-dev
 
+# install libQt5Multimedia library
 sudo apt install qtmultimedia5-dev
+
+# install Apache server 
+sudo apt update
+sudo apt install apache2
+systemctl status apache2
+sudo apt install npm
+sudo npm install -g http-server
